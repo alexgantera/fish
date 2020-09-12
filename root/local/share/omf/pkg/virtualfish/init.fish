eb80e24ed0bd26c8d486d24d8a69fb9c56a9f0d0
Ganter <alexgantera@gmail.com>
2020-09-12 19:42:28 +0300

    update

---
+++ root/local/share/omf/pkg/virtualfish/init.fish
@@ -0,0 +1,12 @@
+if set -q $VIRTUALFISH_PYTHON
+    set VIRTUALFISH_PYTHON python
+end
+
+eval (eval $VIRTUALFISH_PYTHON -m virtualfish $VIRTUALFISH_PLUGINS 2>/dev/null)
+
+if test $status -gt 0
+    echo "Please install virtualfish by running `pip install virtualfish`"
+    echo "and make sure you've set $VIRTUALFISH_PYTHON to the correct"
+    echo "interpreter"
+    echo "See http://virtualfish.readthedocs.io/en/latest/install.html"
+end
